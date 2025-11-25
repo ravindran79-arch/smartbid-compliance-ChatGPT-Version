5c3a45584d83391c08205cd4d6b99371e33a4f98
ravindran79-arch <ravindran.79@gmail.com>
2025-11-25 09:19:00 -0800

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1,7 +1,6 @@
 /* server.js - The Security Bodyguard */
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
